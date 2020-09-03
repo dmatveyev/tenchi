6d54d62c057aac56a4a0359898d8ff83cf369f57
Denis Matveev <matveev@haulmont.com>
2020-09-04 00:04:27 +0400

    Центрирование элементов заголовка. Иконки ссылок на соц сети.

--- src/component/header/Contacts.jsx
+++ src/component/header/Contacts.jsx
@@ -8,8 +8,10 @@ class Contacts extends React.Component{
         return(
 
                 <div className={styles.contacts}>
-                    <li><text>Самара, ул Пугачевская 21а</text></li>
-                    <li><text> 8 (999) 999-99-99</text></li>
+                    <div className={styles.block}>
+                        <li><text>Самара, ул Пугачевская 21а</text></li>
+                        <li><text> 8 (999) 999-99-99</text></li>
+                    </div>
                 </div>
 
 
--- src/component/header/Links.jsx
+++ src/component/header/Links.jsx
@@ -7,12 +7,15 @@ class Links extends React.Component {
     render() {
         return (
                 <div className={styles.links}>
-                    <li>
-                        <a href="https://vk.com/tenchi_samara" target="_blank">Мы в Контакте</a>
-                    </li>
-                    <li>
-                        <a href="https://www.instagram.com/aikido_tenchi" target="_blank">Мы в Инстаграм</a>
-                    </li>
+                    <div className={styles.block}>
+                        <a href="https://vk.com/tenchi_samara" target="_blank" rel="noopener noreferrer">
+                            <img src="vk.png" alt="vk"/>
+                        </a>
+
+                        <a href="https://www.instagram.com/aikido_tenchi" target="_blank" rel="noopener noreferrer">
+                            <img src="insta.png" alt="vk"/>
+                        </a>
+                    </div>
                 </div>
 
         )
