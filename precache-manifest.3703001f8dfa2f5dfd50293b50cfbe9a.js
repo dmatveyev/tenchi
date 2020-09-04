05d22a23f8265676fdbb68471916e1feb77abd3e
denis.a.matveev <denis.a.matveev@rtlabs.com>
2020-09-04 11:02:50 +0400

    Updates

---
+++ precache-manifest.3703001f8dfa2f5dfd50293b50cfbe9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59e8d06d38d7fd9669d042ae92bb9a3",
+    "url": "/tenchi/index.html"
+  },
+  {
+    "revision": "bf6748c89e5770075741",
+    "url": "/tenchi/static/css/main.782e66c5.chunk.css"
+  },
+  {
+    "revision": "14bc46d628a933595fa1",
+    "url": "/tenchi/static/js/2.63993f27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tenchi/static/js/2.63993f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6748c89e5770075741",
+    "url": "/tenchi/static/js/main.4f09d42e.chunk.js"
+  },
+  {
+    "revision": "32ba2655df06d4e0ed7f",
+    "url": "/tenchi/static/js/runtime-main.ac52a934.js"
+  },
+  {
+    "revision": "4e858f4e54d5d1c6b64218845f89b266",
+    "url": "/tenchi/static/media/shomen.4e858f4e.jpg"
+  }
+]);
